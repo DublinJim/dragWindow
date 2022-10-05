6554f829d5735449952d8db7644604950adb67d6
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-10-05 14:10:23 -0400

    vars set

--- src/main/java/com/example/dragwindow/Controller.java
+++ src/main/java/com/example/dragwindow/Controller.java
@@ -1,6 +1,7 @@
 package com.example.dragwindow;
 
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -8,11 +9,21 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-
     public VBox root;
+    private double offsetX = 0.0;
+    private double offsetY = 0.0;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+
+    public void vboxDragged(MouseEvent mouseEvent) {
+
+    }
+
+    public void VboxPressed(MouseEvent mouseEvent) {
 
     }
 }
